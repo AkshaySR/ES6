c7212994ffc4589a150ab7f1f35c51b6da7527d6
Akshay <asr.asrcode@gmail.com>
2018-11-24 16:41:49 -0500

    let and const

---
+++ variableDeclarations/letConst.js
@@ -0,0 +1,11 @@
+//var car = "subaru";
+//var price = 2300;
+
+
+
+//ES6
+const car = "subaru"; //since the name of the car is not going to change
+let price = 2300; // price might change in future
+
+console.log(car);
+console.log(price);
